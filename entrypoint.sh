326e7998507f8448fa4a1c44fea94b225340a9ee
Patrick Rizzardi <35274364+RedactDigital@users.noreply.github.com>
2024-05-21 18:40:10 -0400

    [Tue May 21 18:40:10 EDT 2024] add error handling in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-set -ex
+blue_text='\033[0;36m'
+red_text='\033[0;31m'
+color_reset='\033[0m'
+environment=""
 
-ORGANIZATION_NAME $ORGANIZATION_NAME
-GITHUB_ACCESS_TOKEN $TOKEN
+set -e
+
+# Ensure organization name is set and not empty
+if [ -z "$ORGANIZATION_NAME" ]; then
+    echo -e "${red_text}Organization name is not set. Exiting...${color_reset}"
+    exit 1
+fi
+
+# Ensure access token is set and not empty
+if [ -z "$GITHUB_ACCESS_TOKEN" ]; then
+    echo -e "${red_text}GitHub access token is not set. Exiting...${color_reset}"
+    exit 1
+fi
 
 RUNNER_TOKEN=$(
     curl -X POST \
@@ -13,12 +27,16 @@ RUNNER_TOKEN=$(
         jq .token --raw-output
 )
 
-cd /home/docker/actions-runner
-
 $WORKDIR/config.sh --url https://github.com/${ORGANIZATION_NAME} --token ${RUNNER_TOKEN}
 
+# If we get Cannot configure the runner because it is already configured. To reconfigure the runner, run 'config.cmd remove' or './config.sh remove' first. then we need to remove the runner and re-run the config.sh
+if [ $? -ne 0 ]; then
+    cleanup
+    $WORKDIR/config.sh --url https://github.com/${ORGANIZATION_NAME} --token ${RUNNER_TOKEN}
+fi
+
 cleanup() {
-    echo "Removing runner... "
+    echo -e "${blue_text}Removing runner...${color_reset}"
     $WORKDIR/config.sh remove --unattended --token ${RUNNER_TOKEN}
 }
 
